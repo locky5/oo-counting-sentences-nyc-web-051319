2fdf0cc6f55f753a34333b0c9cf80420ad380ad0
Kevin Dai <daikevin19@yahoo.com>
2019-05-15 14:31:31 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1]=="."
+      true
+    else 
+      false 
+    end
   end
 
   def question?
-
+    if self[-1]=="?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1]=="!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    no_empty_strings = array.reject { |word| word.empty? }
+    no_empty_strings.size
   end
 end
