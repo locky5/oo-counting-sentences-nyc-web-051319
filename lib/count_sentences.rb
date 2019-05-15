72090a4edb4595177ac51e2d69679be103f017bd
Kevin Dai <daikevin19@yahoo.com>
2019-05-15 14:25:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,9 @@ class String
   end
 
   def count_sentences
-
+    first_split = self.split(".")
+    second_split = first_split.split("?")
+    third_split = second_split.split("!")
+    third.split.size
   end
 end
